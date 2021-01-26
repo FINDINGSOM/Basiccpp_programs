6b4e7f39905f30ebae240955f0731e7cb8c3eddf
FINDINGSOM <50260279+FINDINGSOM@users.noreply.github.com>
2021-01-26 21:25:47 +0530

    Create swapwithoutthirdvar.cpp

---
+++ swapwithoutthirdvar.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int a;
+int b;
+cin>>a>>b;
+//before swapping
+cout<<a<<b;
+a=a+b;
+b=a-b;
+a=a-b;
+//after swapping
+cout<<a<<b;
+
+	
+	
+	
+}
