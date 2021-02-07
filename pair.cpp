ce964ab78a8541e9aba3bf6724b4b0986bb25d93
FINDINGSOM <50260279+FINDINGSOM@users.noreply.github.com>
2021-02-07 17:40:30 +0530

    Create pair.cpp

---
+++ pair.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(pair<int,int>p1,pair<int,int>p2){
+	return p1.first<p2.first;
+}
+int main(){
+	
+	int arr[]={3,5,6,7,8,4,2,1};
+	vector <pair<int,int > >v;
+	
+	for(int i=0;i<((sizeof arr)/(sizeof arr[0]));i++){
+		v.push_back(make_pair(arr[i],i));
+		
+	}
+	sort(v.begin(),v.end(),compare);
+	for(int i=0;i<v.size();i++){
+		arr[v[i].second]=i;
+	}
+	for(int i=0;i<v.size();i++){
+		cout<<arr[i];
+	}
+	return 0;
+}
+	
